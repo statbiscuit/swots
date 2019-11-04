ea2eaf0c4ac25dcd17f827155bf969afdba8d8ad
cmjt <cmjonestodd@gmail.com>
2019-11-04 16:48:41 +1300

    adding materials

---
+++ pdqr.r
@@ -0,0 +1,48 @@
+png("pdqr.png",width = 1000,height = 500)
+par(xpd = TRUE,mfrow = c(1,2), mar = c(4,2,0,3))
+## Normal dist.
+set.seed(1234)
+x <- sort(rnorm(1000))
+plot(x,dnorm(x),type = "l",axes = FALSE,xlab = "",ylab = "",lwd = 3)
+n <- 200
+p <- x[n]
+## rnorm
+col <- grey.colors(1,alpha = 0.1)
+h <- hist(x,add = TRUE,freq = FALSE,border = col,cex = 2,col = col)
+text(h$breaks[14],h$density[10],cex = 2,"rnorm")
+arrows(h$breaks[13],h$density[10],h$breaks[11],h$density[10],col = 'lightgrey',lwd = 3)
+## pnorm
+polygon(c(x[x <= p],rev(x[x <= p])),c(rep(0,length(x[x<=p])),dnorm(rev(x[x<=p]))),col = "grey")
+text(x[n/2],dnorm(x[n/2])/2,"pnorm",cex = 2)
+## qnorm
+arrows(p,0,p,dnorm(p),lwd = 3,lty = 2,code = 0)
+text(p,0 - 0.025,"qnorm",cex = 2)
+## dnorm
+arrows(min(x),dnorm(p),p,dnorm(p),code = 0, lwd = 3,lty = 3)
+text(min(x) - 0.5,dnorm(p),"dnorm",cex = 2,srt = 90)
+## redo dnorm
+lines(x,dnorm(x),lwd = 3)
+
+## Poisson
+set.seed(4321)
+lam <- 2.5
+x <- sort(rpois(1000,lam))
+b <- barplot(dpois(unique(x),lam),axes = FALSE,col = col,border = col,
+             ylim = c(-0.03,max(dpois(unique(x),lam))) + 0.02)
+points(b[,1],dpois(unique(x),lam),cex = 3,pch = 20)
+## rpois
+text(b[8,1],dpois(unique(x)[5],lam),"rpois",cex = 2)
+arrows(b[8,1],dpois(unique(x)[5],lam) - 0.01,
+       b[6,1] + 0.5,dpois(unique(x)[7],lam) + 0.01,col = "lightgrey",lwd = 3)
+## dpois
+text(b[8,1],dpois(unique(x)[4],lam),"dpois",cex = 2)
+arrows(b[7,1],dpois(unique(x)[4],lam),b[4,1],dpois(unique(x)[4],lam),code = 0,lwd = 3, lty = 3)
+## qpois
+text(b[4,1],-0.01,"qpois",cex = 2)
+arrows(b[4,1],dpois(unique(x)[4],lam),b[4,1],0,code = 0,lwd = 3, lty = 2)
+## ppois
+barplot(dpois(unique(x)[1:2],lam),col = "grey",add = TRUE,border = "grey",axes = FALSE)
+text(mean(b[1:2,1]),dpois(unique(x)[1],lam)/2,"ppois",cex = 2)
+## redo dpois
+points(b[,1],dpois(unique(x),lam),cex = 3,pch = 20)
+dev.off()
---
+++ weird_coin.r
@@ -0,0 +1,77 @@
+library(png)
+require(animation)
+pngs <- list.files("coin_pics",".png",full = TRUE)
+imgs <- lapply(pngs,png::readPNG)
+
+ani.options(interval = 0.5,ani.width = 1800, ani.height = 900)
+
+saveGIF({
+    layout(matrix(c(0,0,2,2,2,3,3,3,1,1,2,2,2,3,3,3,1,1,2,2,2,3,3,3,0,0,2,2,2,3,3,3),nrow = 4,byrow = TRUE))
+    par(xpd = TRUE)
+    for(i in 1:2){
+        plot(1,type = "n", axes = FALSE,xlim = c(-0.6,0.6),xlab ="",ylim = c(-0.6,0.6),ylab = "")
+        rasterImage(imgs[[i]],-0.5,-0.5,0.5,0.5)
+        text(0,0.6,"Throw a coin 10 times",cex = 4)
+        text(0,-0.6,"Is it fair?",cex = 4)
+        num <- seq(0,40,4)
+        plot(1,type = "n", axes = FALSE,xlim = c(-1,42),xlab ="",ylim = c(-0.1,2.2), ylab = "")
+        ## sleeping
+        for(i in num[2:11]){ rasterImage(imgs[[1]],i - 2,0.1,i + 2,0.3)}
+        for(i in num[3:11]){ rasterImage(imgs[[1]],i - 2,0.3,i + 2,0.5)}
+        for(i in num[4:11]){ rasterImage(imgs[[1]],i - 2,0.5,i + 2,0.7)}
+        for(i in num[5:11]){ rasterImage(imgs[[1]],i - 2,0.7,i + 2,0.9)}
+        for(i in num[6:11]){ rasterImage(imgs[[1]],i - 2,0.9,i + 2,1.1)}
+        for(i in num[7:11]){ rasterImage(imgs[[1]],i - 2,1.1,i + 2,1.3)}
+        for(i in num[8:11]){ rasterImage(imgs[[1]],i - 2,1.3,i + 2,1.5)}
+        for(i in num[9:11]){ rasterImage(imgs[[1]],i - 2,1.5,i + 2,1.7)}
+        for(i in num[10:11]){ rasterImage(imgs[[1]],i - 2,1.7,i + 2,1.9)}
+        for(i in num[11]){ rasterImage(imgs[[1]],i - 2,1.9,i + 2,2.1)}
+        mtext(paste(0:10,"\n sleeping \n cats"),line = 0,side = 1, at = num, cex = 0.9)
+        ## behind
+        for(i in num[1:10]){ rasterImage(imgs[[2]],i - 2,1.9,i + 2,2.1)}
+        for(i in num[1:9]){ rasterImage(imgs[[2]],i - 2,1.7,i + 2,1.9)}
+        for(i in num[1:8]){ rasterImage(imgs[[2]],i - 2,1.5,i + 2,1.7)}
+        for(i in num[1:7]){ rasterImage(imgs[[2]],i - 2,1.3,i + 2,1.5)}
+        for(i in num[1:6]){ rasterImage(imgs[[2]],i - 2,1.1,i + 2,1.3)}
+        for(i in num[1:5]){ rasterImage(imgs[[2]],i - 2,0.9,i + 2,1.1)}
+        for(i in num[1:4]){ rasterImage(imgs[[2]],i - 2,0.7,i + 2,0.9)}
+        for(i in num[1:3]){ rasterImage(imgs[[2]],i - 2,0.5,i + 2,0.7)}
+        for(i in num[1:2]){ rasterImage(imgs[[2]],i - 2,0.3,i + 2,0.5)}
+        for(i in num[1]){ rasterImage(imgs[[2]],i - 2,0.1,i + 2,0.3)}
+        mtext(paste(10:0,"\n teasing \n cats"),line = 0,side = 3, at = num, cex = 0.9)
+        ## boxes
+        rect(xleft = c(-2,38),ybottom = 0.1,xright = c(2,42),ytop = 2.1,border = "red",lwd = 3)
+        rect(xleft = c(2.1,34),ybottom = 0.1,xright = c(6,37.9),ytop = 2.1,border = "red",lwd = 2)
+        rect(xleft = c(6.1,34),ybottom = 0.1,xright = c(10,30),ytop = 2.1,border = "red",lwd = 1)
+        mtext("more \n weird",line = -4,side = 3,at = num[c(1,11)],cex = 1,col = "red")
+        mtext("kind of \n weird",line = -4,side = 3,at = num[c(2,10)],cex = 0.8,col = "red")
+        mtext("less \n weird",line = -4,side = 3,at = num[c(3,9)],cex = 0.6,col = "red")
+        ## line
+        arrows(-2,-0.01,42,-0.01,length = 0.1,code = 3,lwd = 2)
+        text(20,0.05,"expected",cex = 1.7)
+        text(c(8,32),0.05,"weirder",cex = 1.7)
+        ## probabilities
+        ps <- dbinom(0:10,10,0.5)
+        par(xpd = TRUE,mar = c(13.1, 4.1, 4.1, 2.1))
+        bar <- barplot(ps,axes = FALSE,border = NA,ylab = "Probability of getting x cats",cex.lab = 1.5)
+        barplot(c(ps[1],0,0,0,0,0,0,0,0,0,ps[11]),axes = FALSE,border = c("red",NA,NA,NA,NA,NA,NA,NA,NA,NA,"red"),
+                col = NA,lwd = 3,add = TRUE)
+        barplot(c(0,ps[2],0,0,0,0,0,0,0,ps[10],0),axes = FALSE,border = c(NA,"red",NA,NA,NA,NA,NA,NA,NA,"red",NA),
+                col = NA,lwd = 2,add = TRUE)
+        barplot(c(0,0,ps[3],0,0,0,0,0,ps[9],0,0),axes = FALSE,border = c(NA,NA,"red",NA,NA,NA,NA,NA,"red",NA,NA),
+                col = NA,lwd = 1,add = TRUE)
+        ## line
+        arrows(0,-0.01,13,-0.01,length = 0.1,code = 3,lwd = 2)
+        text(7.1,-0.007,"expected",cex = 1.7)
+        text(c(3.1,10.7),-0.007,"weirder",cex = 1.7)
+        ## text
+        text(bar[c(1,11)],0.01,"more \n weird",cex = 1.9,col = "red")
+        text(bar[c(2,10)],0.02,"kind of \n weird",cex = 1.7,col = "red")
+        text(bar[c(3,9)],0.05,"less \n weird",cex = 1.5,col = "red")
+        ## axis
+        axis(2,las = 2,at = seq(0,0.25,0.05),cex.axis = 1.3,line = -1)
+        mtext(paste(0:10,"\n teasing \n or \n sleeping \n cats"),line = 9,side = 1, at = bar, cex = 0.9)
+        par(mar = c(5.1, 4.1, 4.1, 2.1))
+    }
+},movie.name = "coin.gif")
+
