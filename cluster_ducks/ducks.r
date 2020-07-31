05c632794d178bd991e1340f1bc46977734066a6
cmjt <cmjt@st-andrews.ac.uk>
2020-07-31 21:54:55 +1200

    proportion calculations

--- cluster_ducks/ducks.r
+++ cluster_ducks/ducks.r
@@ -7,6 +7,27 @@ cluster_ducks <- data.frame(attire  = stringr::str_match(pics,"images/(.*?)-")[,
                             av_red = sapply(rgbs, function(x) mean(c(x[,,1]))),
                             av_green = sapply(rgbs, function(x) mean(c(x[,,2]))),
                             av_blue = sapply(rgbs, function(x) mean(c(x[,,3]))))
+
+
+prop.max <- function(x){
+    ## matrix of index of max RGB values of x
+    mat_max <- apply(x,c(1,2),which.max)
+    ## table of collapsed values
+    tab <- table(c(mat_max))
+    ## proportion of red
+    prop_red <- tab[1]/sum(tab)
+    prop_green <- tab[2]/sum(tab)
+    prop_blue <- tab[3]/sum(tab)
+    return(c(prop_red,prop_green,prop_blue))
+}
+## proportion of r, g, b in each image
+prop <- do.call('rbind',lapply(rgbs,prop.max))
+cluster_ducks$prop_red <- prop[,1]
+cluster_ducks$prop_green <- prop[,2]
+cluster_ducks$prop_blue <- prop[,3]
+
+
+
 library(ggplot2)
 ggplot(cluster_ducks, aes(x = av_red, y = av_green, color = attire)) +
     geom_point()
@@ -14,5 +35,5 @@ ggplot(cluster_ducks, aes(x = av_red, y = av_green, color = attire)) +
 ggplot(cluster_ducks, aes(x = av_red, y = av_blue, color = attire)) +
     geom_point()
 
-
-tst <- apply(rgbs[[1]],3, max)
+ggplot(cluster_ducks, aes(x = prop_blue, y = prop_green, color = attire)) +
+    geom_point()
