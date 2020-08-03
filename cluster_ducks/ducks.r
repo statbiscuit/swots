81bc777b3a1098fc2b008154fc33b71605d050df
cmjt <cmjt@st-andrews.ac.uk>
2020-08-03 16:38:53 +1200

    gg_tile

--- cluster_ducks/ducks.r
+++ cluster_ducks/ducks.r
@@ -1,12 +1,30 @@
 ## list images
-pics <- list.files("images",full = TRUE)
+## pics <- list.files("images",full = TRUE)
 ## read in rgb values of 
-rgbs <- lapply(pics, jpeg::readJPEG)
+## rgbs <- lapply(pics, jpeg::readJPEG)
 
-cluster_ducks <- data.frame(attire  = stringr::str_match(pics,"images/(.*?)-")[,2],
-                            av_red = sapply(rgbs, function(x) mean(c(x[,,1]))),
-                            av_green = sapply(rgbs, function(x) mean(c(x[,,2]))),
-                            av_blue = sapply(rgbs, function(x) mean(c(x[,,3]))))
+load("duck_rgbs.RData")
+library(ggplot2)
+library(patchwork)
+m <- reshape2::melt(t(apply(duck_rgbs[[1]][,,1],2,rev)))
+r <- ggplot(m, aes(x = Var1, y = Var2, fill = value)) +
+    geom_tile() + scale_fill_distiller(palette = "Reds") +
+    theme_void() + theme(legend.position = "none")
+m <- reshape2::melt(t(apply(duck_rgbs[[1]][,,2],2,rev)))
+g <- ggplot(m, aes(x = Var1, y = Var2, fill = value)) +
+    geom_tile() + scale_fill_distiller(palette = "Greens") +
+    theme_void() + theme(legend.position = "none")
+m <- reshape2::melt(t(apply(duck_rgbs[[1]][,,3],2,rev)))
+b <- ggplot(m, aes(x = Var1, y = Var2, fill = value)) +
+    geom_tile() + scale_fill_distiller(palette = "Blues") +
+    theme_void() + theme(legend.position = "none")
+
+cluster_ducks <- data.frame(attire  = stringr::str_match(names(duck_rgbs),"(.*?)-")[,2],
+                            av_red = sapply(duck_rgbs, function(x) mean(c(x[,,1]))),
+                            av_green = sapply(duck_rgbs, function(x) mean(c(x[,,2]))),
+                            av_blue = sapply(duck_rgbs, function(x) mean(c(x[,,3]))))
+
+
 
 
 prop.max <- function(x){
@@ -30,10 +48,48 @@ cluster_ducks$prop_blue <- prop[,3]
 
 library(ggplot2)
 ggplot(cluster_ducks, aes(x = av_red, y = av_green, color = attire)) +
-    geom_point()
+    geom_point() + scale_colour_brewer(type = "qual")
 
 ggplot(cluster_ducks, aes(x = av_red, y = av_blue, color = attire)) +
-    geom_point()
+    geom_point()  + scale_colour_brewer(type = "qual")
 
 ggplot(cluster_ducks, aes(x = prop_blue, y = prop_red, color = attire)) +
-    geom_point()
+    geom_point() + scale_colour_brewer(type = "qual")
+
+ggplot(cluster_ducks, aes(x = prop_blue, y = prop_green, color = attire)) +
+    geom_point() + scale_colour_brewer(type = "qual")
+
+ggplot(cluster_ducks, aes(x = prop_red, y = prop_green, color = attire)) +
+    geom_point() + scale_colour_brewer(type = "qual")
+
+
+devtools::install_github("AckerDWM/gg3D")
+
+library("gg3D")
+
+ggplot(cluster_ducks, aes(x = prop_red, y = prop_green, z = prop_blue, color = attire)) + 
+    axes_3D() +
+    stat_3D()
+
+x11()
+ggplot(cluster_ducks, aes(x = av_red, y = av_green, z = av_blue, color = attire)) + 
+    axes_3D() +
+    stat_3D()
+
+library(plotly)
+
+plot_ly(x = cluster_ducks$prop_red, y = cluster_ducks$prop_green, z = cluster_ducks$prop_blue,
+        type = "scatter3d", mode = "markers", color = cluster_ducks$attire)
+
+plot_ly(x = cluster_ducks$av_red, y = cluster_ducks$av_green, z = cluster_ducks$av_blue,
+        type = "scatter3d", mode = "markers", color = cluster_ducks$attire)
+## k means clustering
+library(factoextra)
+df <- cluster_ducks[,2:7];rownames(df) <- stringr::str_match(pics,"images/(.*?).jp")[,2]
+distance <- get_dist(df)
+fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+
+
+k2 <- kmeans(df, centers = 6, nstart = 25)
+
+fviz_cluster(k2, data = df)
