4f9b9a6c16a8e44d07617baf36cd13914af541d5
cmjt <cmjonestodd@gmail.com>
2020-09-11 13:29:24 +1200

    testing travis

---
+++ swots/_deploy.sh
@@ -0,0 +1,10 @@
+## clone the repository to the book-output directory
+git clone  \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+git rm -rf *
+cp -r ../_book/* ./
+git add --all *
+git commit -m "Update the book"
+git push -q origin master
