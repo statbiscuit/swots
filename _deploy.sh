e3b52c664c681a57de5390595fbf062ace3784be
cmjt <cmjonestodd@gmail.com>
2020-09-11 16:41:18 +1200

    changing deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,15 +1,5 @@
-# configure your name and email if you have not done so
-git config --global user.email "cmjonestodd@gmail.com"
-git config --global user.name "cmjt"
-
-
 ## clone the repository to the book-output directory
-git clone  \
-  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
-  docs
 cd docs
-git rm -rf *
-touch .nojekyll
 cp -r ../swots/_book/* ./
 git add --all *
 git commit -m "update worksheets"
